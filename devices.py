71386868b6b9a29a4a45e33076a09e7abcb22577
i.yurtaev <34nomer@gmail.com>
2022-06-08 12:34:36 +0400

    add menu command

--- devices.py
+++ devices.py
@@ -28,25 +28,53 @@ class ZDV(Device):
         super().__init__()
         for i in range(126):
             self.lst_registers.append(Register16(0))
+        self.dict_functions = {"auto": self.auto_act,
+                           "state": self.set_state,
+                           "registers": self.set_register,
+                               }
 
     def show(self):
         for i in range(5):
             print(f"{i}: {self.lst_registers[i]}")
 
+    def only_show(self):
+        system('cls')  # Очистка экрана
+        self.show()
+        return
+
     def auto_act(self):
+        print("вызвана функция Автоматическое обновление модели")
         pass
-        return 0
+
+    def set_state(self):
+        print("вызвана функция Установить новый параметр")
+        pass
+
+    def set_bit(self):
+        print("вызвана функция Установить бит")
+        pass
+
+    def set_register(self):
+        print("вызвана функция установить новое значение регистра")
 
     def management(self):
-        system('cls') # Очистка экрана
-        self.show()
-        while True:
-            command = input("""Введите команду в формате 
-            <номер регистра>[<номер бита>]=<значение> — для изменения бита
-            <номер регистра>=<значение> — для изменения значения
-            q-для выхода""")
-            if "q" in command.lower():
-                break
+
+        command = ""
+        while command.lower() != "q":
+            self.only_show()
+            for function in self.dict_functions:
+                print("q - для выхода ")
+                print("выберите функцию")
+                print(f"{function}: enter the command ")
+            command = input()
+            for function in self.dict_functions:
+                if command.lower() == function:
+                    self.dict_functions[function]()
+            if command.lower() == "q":
+                continue
+            else:
+                print("Неизвестная команда")
+
 
 
 
