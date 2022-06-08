7f70af20d8e8777e4f355a9df42eb199936ea2c6
i.yurtaev <34nomer@gmail.com>
2022-06-08 13:58:38 +0400

    add empty commands

--- devices.py
+++ devices.py
@@ -7,8 +7,8 @@ class Device(object):
     def __init__(self):
         self.lst_registers = []
         self.dct_settings = {}
-        self.output_param = {}
-        self.input_param = {}
+        self.state = {}
+
 
     @abstractmethod
     def show(self):
@@ -29,9 +29,17 @@ class ZDV(Device):
         for i in range(126):
             self.lst_registers.append(Register16(0))
         self.dict_functions = {"auto": self.auto_act,
-                           "state": self.set_state,
-                           "registers": self.set_register,
+                               "command": self.get_command,
+                               "registers": self.set_register,
                                }
+        self.dict_commands = {"open": self.open,
+                              "close": self.close,
+                              "in_between": self.set_in_between,
+                              "set_opened": self.open,
+                              "set_closed": self.close,
+                              }
+        self.state = {}
+        self.possible_state = {}
 
     def show(self):
         for i in range(5):
@@ -39,6 +47,7 @@ class ZDV(Device):
 
     def only_show(self):
         system('cls')  # Очистка экрана
+        print("Должен быть чистый экран")
         self.show()
         return
 
@@ -46,9 +55,23 @@ class ZDV(Device):
         print("вызвана функция Автоматическое обновление модели")
         pass
 
-    def set_state(self):
+    def get_command(self):
         print("вызвана функция Установить новый параметр")
-        pass
+        user_input = ""
+        while user_input.lower() != "q":
+            self.only_show()
+            print("q - для выхода ")
+            for command in self.dict_commands:
+                print("выберите функцию")
+                print(f"{command} ")
+            user_input = input()
+            for command in self.dict_commands:
+                if user_input.lower() == command:
+                    self.dict_commands[command]()
+            if command.lower() == "q":
+                continue
+            else:
+                print("Неизвестная команда")
 
     def set_bit(self):
         print("вызвана функция Установить бит")
@@ -57,20 +80,38 @@ class ZDV(Device):
     def set_register(self):
         print("вызвана функция установить новое значение регистра")
 
+    def open(self):
+        print("вызвана функция открыть задвижку")
+
+    def close(self):
+        print("вызвана функция закрыть задвижку")
+
+    def stop(self):
+        print("вызвана функция остановить задвижку")
+
+    def set_in_between(self):
+        print("вызвана функция установить промежуток")
+
+    def set_opened(self):
+        print("вызвана функция установить задвижку в положение 'Открыто'")
+
+    def set_closed(self):
+        print("вызвана функция установить задвижку в положение 'Закрыто'")
+
     def management(self):
 
-        command = ""
-        while command.lower() != "q":
+        user_input = ""
+        while user_input.lower() != "q":
             self.only_show()
+            print("q - для выхода ")
+            print("выберите функцию")
             for function in self.dict_functions:
-                print("q - для выхода ")
-                print("выберите функцию")
-                print(f"{function}: enter the command ")
-            command = input()
+                print(f"{function}")
+            user_input = input()
             for function in self.dict_functions:
-                if command.lower() == function:
+                if user_input.lower() == function:
                     self.dict_functions[function]()
-            if command.lower() == "q":
+            if user_input.lower() == "q":
                 continue
             else:
                 print("Неизвестная команда")
@@ -79,6 +120,8 @@ class ZDV(Device):
 
 
 
+zdv1 = ZDV()
 
+zdv1.management()
 
 
